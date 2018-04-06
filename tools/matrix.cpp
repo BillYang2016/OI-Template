99340894601df072cc4a0feed682d066c4e4a915
Bill_Yang_2016 <ym13883407586@outlook.com>
2018-04-06 15:20:18 +0800

    update matrix

--- tools/matrix.cpp
+++ tools/matrix.cpp
@@ -33,28 +33,26 @@ struct Matrix {
 	LL a[maxn][maxn];
 	Matrix(int n=0,int m=0,bool f=0):n(n),m(m) {
 		memset(a,0,sizeof(a));
-		for(int i=1; i<=n; i++)a[i][i]=f;
+		for(int i=0; i<n; i++)a[i][i]=f;
 	}
-	LL* operator [] (const int x) {
-		return a[x];
-	}
+	LL* operator [] (const int x) {return a[x];}
 	Matrix T() {
 		Matrix c(m,n);
-		for(int i=1; i<=n; i++)
-			for(int j=1; j<=m; j++)c[j][i]=a[i][j];
+		for(int i=0; i<n; i++)
+			for(int j=0; j<m; j++)c[j][i]=a[i][j];
 		return c;
 	}
 	Matrix operator + (Matrix b) {
 		Matrix c(n,m);
-		for(int i=1; i<=n; i++)
-			for(int j=1; j<=m; j++)c[i][j]=a[i][j]+b[i][j]>=mod?a[i][j]+b[i][j]-mod:a[i][j]+b[i][j];
+		for(int i=0; i<n; i++)
+			for(int j=0; j<m; j++)c[i][j]=a[i][j]+b[i][j]>=mod?a[i][j]+b[i][j]-mod:a[i][j]+b[i][j];
 		return c;
 	}
 	Matrix operator * (Matrix b) {
 		Matrix c(n,b.m);
-		for(int i=1; i<=n; i++)
-			for(int j=1; j<=b.m; j++) {
-				for(int k=1; k<=m; k++)c[i][j]+=a[i][k]*b[k][j];
+		for(int i=0; i<n; i++)
+			for(int j=0; j<b.m; j++) {
+				for(int k=0; k<m; k++)c[i][j]+=a[i][k]*b[k][j];
 				c[i][j]%=mod;
 			}
 		return c;
