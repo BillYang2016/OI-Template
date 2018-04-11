e6d56622e96482f437a5f88c5816c8cd733c855f
Bill_Yang_2016 <ym13883407586@outlook.com>
2018-04-11 19:04:06 +0800

    rename

--- algorithm/linear bases/linear-bases.cpp
+++ algorithm/linear bases/linear-bases.cpp
@@ -12,25 +12,25 @@ using namespace std;
 
 typedef long long LL;
 
-const int maxn=20005,MAX_BASE=60;
+const int K=60;
 
 struct Linear_Bases {
-	LL b[MAX_BASE+5];
-	Linear_Bases() {fill(b,b+MAX_BASE+1,0);}
+	LL b[K+5];
+	Linear_Bases() {fill(b,b+K+1,0);}
 	void add(LL num) {
-		for(int j=MAX_BASE; j>=0; j--)if(num>>j&1) {
+		for(int j=K; j>=0; j--)if(num>>j&1) {
 			if(b[j]) {num^=b[j];continue;} //already had bases
 			b[j]=num;
 			for(int k=j-1; k>=0; k--)if(b[j]>>k&1)b[j]^=b[k];
-			for(int k=j+1; k<=MAX_BASE; k++)if(b[k]>>j&1)b[k]^=b[j];
+			for(int k=j+1; k<=K; k++)if(b[k]>>j&1)b[k]^=b[j];
 			break;
 		}
 	}
 	void build(vector<int> a) {for(int num:a)add(num);}
-	void merge(const Linear_Bases& b) {for(int i=0; i<=MAX_BASE; i++)if(b.b[i])add(b.b[i]);}
+	void merge(const Linear_Bases& b) {for(int i=0; i<=K; i++)if(b.b[i])add(b.b[i]);}
 	LL cal() { //max xor-sum
 		LL ans=0;
-		for(int i=0; i<=MAX_BASE; i++)ans^=b[i];
+		for(int i=0; i<=K; i++)ans^=b[i];
 		return ans;
 	}
 };
